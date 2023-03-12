f9999b5b0eea92879a68edbbcbb795b1cfe232e0
Aldrin Jenson <aldrinjenson@gmail.com>
2023-03-12 14:27:27 +0530

    add image modal card

---
+++ components/ImageViewer.js
@@ -0,0 +1,27 @@
+import * as React from "react";
+import { View, Image } from "react-native";
+
+const ImageViewer = ({ imgUrl }) => {
+  console.log(imgUrl);
+
+  return (
+    <View
+      style={{
+        // maxWidth: "90%",
+        maxHeight: "90%",
+      }}
+    >
+      <Image
+        // style={styles.tinyLogo}
+        // source={require("@expo/snack-static/react-native-logo.png")}
+        style={{
+          width: "100%",
+          height: "100%",
+        }}
+        source={{ uri: imgUrl }}
+      />
+    </View>
+  );
+};
+
+export default ImageViewer;
